eeb0753285b4de86c822b3f97d1acc238fa4a1cd
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-01-26 10:05:31 -0300

    half screen slide down menu placed

--- ableton.js
+++ ableton.js
@@ -1,4 +1,4 @@
-// Get the header element
+// get the secondary-nav
 var header = document.getElementById("secondary-nav");
 
 // Get the offset position of the header
@@ -15,3 +15,15 @@ function handleScroll() {
 
 // Add event listener to the window's scroll event
 window.addEventListener("scroll", handleScroll);
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+  document.getElementById('menu').classList.toggle('open');
+  document.getElementById('menu-toggle').classList.toggle('open');
+  document.getElementById('close-button').classList.toggle('open');
+});
+
+document.getElementById('close-button').addEventListener('click', function() {
+  document.getElementById('menu').classList.remove('open');
+  document.getElementById('menu-toggle').classList.remove('open');
+  document.getElementById('close-button').classList.remove('open');
+});
